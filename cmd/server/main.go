d44f3b101ae87690c5e5bb48d12b14417c09f603
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-08-19 20:17:59 -0300

    feat: enhance RabbitMQ server setup with connection handling and signal interruption

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	rabbitMQConnectionString := "amqp://guest:guest@localhost:5672/" // update later to use env variable
+
+	conn, err := amqp091.Dial(rabbitMQConnectionString)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ successfully!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	<-signalChan
+	fmt.Println("Received interrupt signal. Shutting down...")
 }
