1a89b6110edbd78543789345c61a9ce1d99c8926
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-08-25 17:15:03 -0300

    feat: refactor subscription handling for pause and army moves in client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,15 +8,8 @@ import (
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	"github.com/joho/godotenv"
+	"github.com/rabbitmq/amqp091-go"
 )
-
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
-	return func(state routing.PlayingState) {
-		defer fmt.Print("> ")
-
-		gs.HandlePause(state)
-	}
-}
 
 func main() {
 	fmt.Println("Starting Peril client...")
@@ -41,10 +34,15 @@ func main() {
 
 	gameState := gamelogic.NewGameState(userName)
 
-	queueName := routing.PauseKey + "." + userName
-	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.QueueTransient, handlerPause(gameState))
+	err = SubscribeToPause(userName, err, conn, gameState)
 	if err != nil {
-		fmt.Println("Failed to subscribe to pause messages:", err)
+		fmt.Println("Error subscribing to pause messages:", err)
+		return
+	}
+
+	err = SubscribeToArmyMoves(userName, err, conn, gameState)
+	if err != nil {
+		fmt.Println("Error subscribing to army moves messages:", err)
 		return
 	}
 
@@ -58,10 +56,12 @@ func main() {
 				fmt.Println("Error spawning unit:", err)
 			}
 		case "move":
-			_, err := gameState.CommandMove(words)
+			move, err := gameState.CommandMove(words)
 			if err != nil {
 				fmt.Println("Error moving unit:", err)
 			}
+			pubsub.PublishJSON(channel, routing.ExchangePerilTopic, "army_moves."+userName, move)
+			fmt.Println("Move command sent to server.")
 		case "status":
 			gameState.CommandStatus()
 		case "help":
@@ -75,4 +75,40 @@ func main() {
 			fmt.Println("Unknown command. Type 'help' for a list of commands.")
 		}
 	}
+}
+
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
+	return func(state routing.PlayingState) {
+		defer fmt.Print("> ")
+
+		gs.HandlePause(state)
+	}
+}
+
+func SubscribeToPause(userName string, err error, conn *amqp091.Connection, gameState *gamelogic.GameState) error {
+	queueName := routing.PauseKey + "." + userName
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.QueueTransient, handlerPause(gameState))
+	if err != nil {
+		fmt.Println("Error subscribing to pause messages:", err)
+		return err
+	}
+	return nil
+}
+
+func handlerArmyMoves(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+
+		gs.HandleMove(move)
+	}
+}
+
+func SubscribeToArmyMoves(userName string, err error, conn *amqp091.Connection, gameState *gamelogic.GameState) error {
+	queueName := "army_moves" + "." + userName
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, queueName, "army_moves.*", pubsub.QueueTransient, handlerArmyMoves(gameState))
+	if err != nil {
+		fmt.Println("Error subscribing to army moves messages:", err)
+		return err
+	}
+	return nil
 }
