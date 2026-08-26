55348135674925d4678212c1113a4a054bfb6c0d
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-08-26 18:33:27 -0300

    feat: add subscription handling for war messages and enhance army moves processing

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -40,9 +40,16 @@ func main() {
 		return
 	}
 
-	err = SubscribeToArmyMoves(userName, err, conn, gameState)
+	err = SubscribeToArmyMoves(userName, err, conn, channel, gameState)
 	if err != nil {
 		fmt.Println("Error subscribing to army moves messages:", err)
+		return
+	}
+
+	// Subscribe to the shared "war" queue so this client can resolve wars.
+	err = SubscribeToWar(userName, err, conn, gameState)
+	if err != nil {
+		fmt.Println("Error subscribing to war messages:", err)
 		return
 	}
 
@@ -99,12 +106,26 @@ func SubscribeToPause(userName string, err error, conn *amqp091.Connection, game
 	return nil
 }
 
-func handlerArmyMoves(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+func handlerArmyMoves(gs *gamelogic.GameState, ch *amqp091.Channel, userName string) func(gamelogic.ArmyMove) pubsub.AckType {
 	return func(move gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
 
 		outcome := gs.HandleMove(move)
-		if outcome == gamelogic.MoveOutComeSafe || outcome == gamelogic.MoveOutcomeMakeWar {
+		if outcome == gamelogic.MoveOutcomeMakeWar {
+			// Publish the war declaration to the topic exchange so war messages are broadcast.
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix+"."+userName, gamelogic.RecognitionOfWar{
+				Attacker: move.Player,
+				Defender: gs.GetPlayerSnap(),
+			})
+			if err != nil {
+				fmt.Println("Error publishing war declaration:", err)
+				return pubsub.NackRequeue
+			}
+
+			return pubsub.Ack
+		}
+
+		if outcome == gamelogic.MoveOutComeSafe {
 			return pubsub.Ack
 		}
 
@@ -112,11 +133,47 @@ func handlerArmyMoves(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.A
 	}
 }
 
-func SubscribeToArmyMoves(userName string, err error, conn *amqp091.Connection, gameState *gamelogic.GameState) error {
+func SubscribeToArmyMoves(userName string, err error, conn *amqp091.Connection, ch *amqp091.Channel, gameState *gamelogic.GameState) error {
 	queueName := "army_moves" + "." + userName
-	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, queueName, "army_moves.*", pubsub.QueueTransient, handlerArmyMoves(gameState))
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, queueName, "army_moves.*", pubsub.QueueTransient, handlerArmyMoves(gameState, ch, userName))
 	if err != nil {
 		fmt.Println("Error subscribing to army moves messages:", err)
+		return err
+	}
+	return nil
+}
+
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+		defer fmt.Print("> ")
+
+		outcome, _, _ := gs.HandleWar(rw)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			// This client isn't in the war; requeue so another client can try to resolve it.
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			// No overlapping units, so there's nothing to fight; discard the message.
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
+		default:
+			// Unknown outcome; print an error and discard.
+			fmt.Println("Unknown war outcome...")
+			return pubsub.NackDiscard
+		}
+	}
+}
+
+func SubscribeToWar(userName string, err error, conn *amqp091.Connection, gameState *gamelogic.GameState) error {
+	// All clients share this durable "war" queue, bound to war.* on the topic exchange.
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, "war", routing.WarRecognitionsPrefix+".*", pubsub.QueueDurable, handlerWar(gameState))
+	if err != nil {
+		fmt.Println("Error subscribing to war messages:", err)
 		return err
 	}
 	return nil
